cfc4ef2a758cec93271e1786163ee14592e465ab
NiJia <louis70109@gmail.com>
2020-08-11 14:34:27 +0800

    feat: Unsend event handle

--- controller/line_controller.py
+++ controller/line_controller.py
@@ -7,7 +7,7 @@ from linebot import (
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, VideoSendMessage, TextSendMessage)
-from linebot.models.events import VideoPlayCompleteEvent
+from linebot.models.events import VideoPlayCompleteEvent, UnsendEvent
 
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
@@ -38,15 +38,29 @@ class LineIconSwitchController(Resource):
             messages=TextSendMessage(text="阿龜可愛ㄇ")
         )
 
-    # do something
+    @handler.add(UnsendEvent)
+    def handle_follow(event):
+        print(event)
+        if event.source.group_id:
+            line_bot_api.push_message(to=event.source.group_id, messages=TextMessage(text="偷偷收回喔"))
+        elif event.source.room_id:
+            line_bot_api.push_message(to=event.source.room_id, messages=TextMessage(text="偷收回？"))
+
     @handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
+        print(event)
         text = event.message.text
         user_id = event.source.user_id
-        line_bot_api.reply_message(
-            event.reply_token,
-            messages=VideoSendMessage(
-                original_content_url='https://i.imgur.com/1Os9Wz6.mp4',
-                preview_image_url='https://stickershop.line-scdn.net/stickershop/v1/sticker/52002734/iPhone/sticker_key@2x.png',
-                tracking_id="tttt")
-        )
+        if event.source.type == 'group':
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages=TextSendMessage(text=text)
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages=VideoSendMessage(
+                    original_content_url='https://i.imgur.com/1Os9Wz6.mp4',
+                    preview_image_url='https://stickershop.line-scdn.net/stickershop/v1/sticker/52002734/iPhone/sticker_key@2x.png',
+                    tracking_id="tttt")
+            )
